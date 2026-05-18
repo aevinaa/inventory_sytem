7ab7d64bbbecc79fabbaf8befc87596a527610b7
Aditi <agrawaladiti247@gmail.com>
2026-05-18 16:33:36 +0530

    Fixing CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -36,10 +36,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "http://localhost:5173",
     "https://inventory-system-lyart.vercel.app",
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
